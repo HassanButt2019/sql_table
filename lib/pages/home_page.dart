3bd9ba980ad49ba6e27e8c8bc38860fda4157cd4
Hassan Butt <hassanqa13@gmail.com>
2021-08-07 16:19:06 +0500

    removed overflow

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,39 +19,42 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/table_view_page');
-          }, child: Text("Table View Page")),
-          ElevatedButton(onPressed:(){
-            Navigator.pushNamed(context, '/download_image_folder_page');
-          }, child: Text("Download image folder")),
-          ElevatedButton(onPressed:(){
-            Navigator.pushNamed(context, '/dashboard_page');
-          }, child: Text("Dashboard")),
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/create_account_page');
-          }, child: Text('Create Account')),
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/account_to_group');
-          }, child: Text('Account 2 Group')),
-           ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/account_3_name');
-          }, child: Text('Account 3 Group')),
-           ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/item_2_group');
-          }, child: Text('Item 2 Group')),
-           ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/item_3_name');
-          }, child: Text('Item 3 Name')),
-          ElevatedButton(onPressed: (){
-            Navigator.pushNamed(context, '/cash_book');
-          }, child: Text('Cash Book')),
-
-          
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/table_view_page');
+            }, child: Text("Table View Page")),
+            ElevatedButton(onPressed:(){
+              Navigator.pushNamed(context, '/download_image_folder_page');
+            }, child: Text("Download image folder")),
+            ElevatedButton(onPressed:(){
+              Navigator.pushNamed(context, '/dashboard_page');
+            }, child: Text("Dashboard")),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/create_account_page');
+            }, child: Text('Create Account')),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/account_to_group');
+            }, child: Text('Account 2 Group')),
+             ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/account_3_name');
+            }, child: Text('Account 3 Group')),
+             ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/item_2_group');
+            }, child: Text('Item 2 Group')),
+             ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/item_3_name');
+            }, child: Text('Item 3 Name')),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, '/cash_book');
+            }, child: Text('Cash Book')),
+        
+            
+        
+          ],
+        ),
       ),
     );
   }
